0738afe71a125bba488704d209945ddc8caedc13
Frank <akura11.tt@gmail.com>
2016-06-06 17:37:20 -0500

    Correccion de problema con postgis en ckan postgres

--- ckan-plugins/start.sh
+++ ckan-plugins/start.sh
@@ -3,19 +3,20 @@
 # Adjust config file
 sed -i -e "s|#solr_url = http://127.0.0.1:8983/solr|solr_url = http://$SOLR_PORT_8983_TCP_ADDR:$SOLR_PORT_8983_TCP_PORT/solr|" /project/development.ini
 sed -i -e "s|ckan.site_url =|ckan.site_url = http://localhost|" /project/development.ini
-sed -i -e "s|ckan_default:pass@localhost/ckan_default|$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_POSTGRES_USER|" /project/development.ini
-sed -i -e "s|datastore_default:pass@localhost/datastore_default|$POSTGRES_ENV_POSTUSER_DATASTORE:$POSTGRES_ENV_POSTPWD_DATASTORE@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_POSTUSER_DATASTORE|" /project/development.ini
+sed -i -e "s|ckan_default:pass@localhost/ckan_default|$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_POSTGRES_DB|" /project/development.ini
+sed -i -e "s|datastore_default:pass@localhost/datastore_default|$POSTGRES_ENV_USER_DATASTORE:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_USER_DATASTORE|" /project/development.ini
+sed -i -e "s|hostname:port:database:username:password|$POSTGRES_PORT_5432_TCP_ADDR:5432:$POSTGRES_ENV_POSTGRES_DB:$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD|" /root/.pgpass
 
 # Create tables
 if [ "$INIT_DBS" = true ]; then
   $CKAN_HOME/bin/paster --plugin=ckan db init -c /project/development.ini
-  $CKAN_HOME/bin/paster --plugin=ckan datastore set-permissions -c /project/development.ini | ssh $POSTGRES_PORT_5432_TCP_ADDR sudo -u postgres psql --username $POSTGRES_USER --set ON_ERROR_STOP=1
+  $CKAN_HOME/bin/paster --plugin=ckan datastore set-permissions -c /project/development.ini | ssh $POSTGRES_PORT_5432_TCP_ADDR sudo -u postgres psql --username $POSTGRES_ENV_POSTGRES_USER --set ON_ERROR_STOP=1
   $CKAN_HOME/bin/paster --plugin=ckanext-spatial spatial initdb 4326 -c /project/development.ini
 fi
 
 # Load a dump file to ckan database
 if [ "$LOAD_DUMP" == true]; then
-    $CKAN_HOME/bin/paster db load -c /project/development.ini $PATH_DUMP_SQL
+  $CKAN_HOME/bin/paster db load -c /project/development.ini $PATH_DUMP_SQL
 fi
 
 # Create test data for development purpose
--- ckan/start.sh
+++ ckan/start.sh
@@ -4,7 +4,6 @@
 sed -i -e "s|#solr_url = http://127.0.0.1:8983/solr|solr_url = http://$SOLR_PORT_8983_TCP_ADDR:$SOLR_PORT_8983_TCP_PORT/solr|" /project/development.ini
 sed -i -e "s|ckan.site_url =|ckan.site_url = http://localhost|" /project/development.ini
 sed -i -e "s|ckan_default:pass@localhost/ckan_default|$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_POSTGRES_USER|" /project/development.ini
-sed -i -e "s|datastore_default:pass@localhost/datastore_default|$POSTGRES_ENV_POSTUSER_DATASTORE:$POSTGRES_ENV_POSTPWD_DATASTORE@$POSTGRES_PORT_5432_TCP_ADDR/$POSTGRES_ENV_POSTUSER_DATASTORE|" /project/development.ini
 
 # Create tables
 if [ "$INIT_DBS" = true ]; then
